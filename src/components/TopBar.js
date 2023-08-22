941f9828a0cbc58a86e7b59926eb94ab9f6395e6
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-21 22:25:49 -0400

    adjust css

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -13,7 +13,7 @@ export default function TopBar () {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto"> 
                     </Nav>
-                    <Nav style={{padding: "24px 40px 20px 1010px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex" }} >
+                    <Nav style={{padding: "24px 40px 20px 750px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex" }} >
                         <Nav.Link style={{color: "#343538"}} href="#LogIn">Log In</Nav.Link>
                         <Button variant="success" style={{fontSize: "18px"}}>Sign Up</Button>{' '}
                     </Nav>
